0b6c238a5a27f2d7d9e528ea7138024d3da82750
Trung Pham <phamtrung101004@gmail.com>
2024-05-23 19:39:02 +0700

    no no

--- src/main/java/com/example/DES/controller/Controller.java
+++ src/main/java/com/example/DES/controller/Controller.java
@@ -4,8 +4,13 @@ import com.example.DES.entity.ContextHolder;
 import com.example.DES.service.DESService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.Base64;
 
 @RestController
@@ -27,5 +32,34 @@ public class Controller {
     @PostMapping(path = "/decrypt")
     public String decrypt(@RequestBody ContextHolder decodeContextHolder) throws Exception {
         return desService.decode(decodeContextHolder.getText(), decodeContextHolder.getSecret_key());
+    }
+    @PostMapping(path = "/get-html")
+    public String getHtml(@RequestParam("file") MultipartFile file) throws Exception{
+        String html = "";
+        try  {
+            File tempFile = File.createTempFile("temp", null);
+            try (OutputStream os = new FileOutputStream(tempFile)) {
+                os.write(file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw e;
+            }
+            FileInputStream fileInputStream = new FileInputStream(tempFile);
+            XWPFDocument document = new XWPFDocument(fileInputStream);
+            StringBuilder htmlContent = new StringBuilder();
+            for (XWPFParagraph paragraph : document.getParagraphs()){
+                htmlContent.append("<p>");
+                for (XWPFRun xwpfRun : paragraph.getRuns()){
+                    String style = desService.getRunStyle(xwpfRun);
+                    htmlContent.append("<span").append(style).append(">")
+                            .append(xwpfRun.text()).append("</span>");
+                }
+                htmlContent.append("</p>");
+            }
+            html = htmlContent.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return html;
     }
 }
--- src/main/java/com/example/DES/service/DESService.java
+++ src/main/java/com/example/DES/service/DESService.java
@@ -1,5 +1,7 @@
 package com.example.DES.service;
 
+import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.Cipher;
@@ -12,6 +14,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.spec.AlgorithmParameterSpec;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Objects;
 
 @Service
 public class DESService {
@@ -46,5 +49,44 @@ public class DESService {
             textByte = Arrays.copyOfRange(textByte, 1, textByte.length);
         }
         return new String(decode.doFinal(textByte));
+    }
+    public String getRunStyle(XWPFRun run) {
+        StringBuilder style = new StringBuilder(" style=\"");
+
+        // Font family
+        String fontFamily = run.getFontFamily();
+        if (fontFamily != null && !fontFamily.isEmpty()) {
+            style.append("font-family: ").append(fontFamily).append(";");
+        }
+        else {
+            style.append("font-family: ").append("Calibri").append(";");
+        }
+
+        // Font size
+        if (run.getFontSizeAsDouble() != null){
+            int fontSize = run.getFontSizeAsDouble().intValue();
+            style.append("font-size: ").append(fontSize).append("pt;");
+        }
+        // Other styling properties (e.g., bold, italic, underline, color, etc.)
+        if (run.isBold()) {
+            style.append("font-weight: bold;");
+        }
+        if (run.isItalic()) {
+            style.append("font-style: italic;");
+        }
+        if (run.isStrikeThrough()) {
+            style.append("text-decoration: line-through;");
+        }
+        if (run.getColor() != null) {
+            style.append("color: #").append(run.getColor()).append(";");
+        }
+        if (run.getUnderline() != UnderlinePatterns.NONE) {
+            style.append("text-decoration: underline;");
+        }
+        if (!Objects.equals(String.valueOf(run.getTextHighlightColor()), "none")){
+            style.append("background-color: ").append(run.getTextHighlightColor()).append(";");
+        }
+        style.append("\"");
+        return style.toString();
     }
 }
